e2e15d302e2083ae2383cc0604e8c232c3303dfe
ThinkCentre <ThinkCentre@JH-Home-ThinkCtntre.dhcp.wustl.edu>
2024-10-02 15:36:20 -0500

    flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,59 @@
 package studio4;
 
-import java.awt.Color;
+//import java.awt.Color;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		/*double r = in.nextDouble();
+		double g = in.nextDouble();
+		double b = in.nextDouble();
+		boolean fill = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double hWidth = in.nextDouble();
+		double hHeight = in.nextDouble();*/
 		
+		//gradients
+		StdDraw.setPenColor(235, 193, 45);
+		StdDraw.filledRectangle(0.5, 0.45, 0.5 , 0.05);
+		StdDraw.setPenColor(219, 148, 42);
+		StdDraw.filledRectangle(0.5, 0.55, 0.5 , 0.05);
+		StdDraw.setPenColor(227, 74, 36);
+		StdDraw.filledRectangle(0.5, 0.65, 0.5 , 0.05);
+		StdDraw.setPenColor(237, 38, 71);
+		StdDraw.filledRectangle(0.5, 0.75, 0.5 , 0.05);
+		
+		
+		//sun
+		StdDraw.setPenColor(237, 234, 40);
+		StdDraw.filledCircle(0.5, 0.4, 0.1);
+		//sea
+		StdDraw.setPenColor(39, 104, 184);
+		StdDraw.filledRectangle(0.5, 0.3, 0.5 , 0.1);
+		
+		//clouds
+		StdDraw.setPenColor(250, 195, 247);
+		StdDraw.filledCircle(0.1, 0.7, 0.05);
+		StdDraw.filledCircle(0.125, 0.72, 0.05);
+		StdDraw.filledCircle(0.15, 0.7, 0.05);
+		StdDraw.filledCircle(0.3, 0.6, 0.05);
+		StdDraw.filledCircle(0.325, 0.62, 0.05);
+		StdDraw.filledCircle(0.35, 0.6, 0.05);
+		StdDraw.filledCircle(0.8, 0.65, 0.05);
+		StdDraw.filledCircle(0.825, 0.67, 0.05);
+		StdDraw.filledCircle(0.85, 0.65, 0.05);
+		
+		//trap
+		StdDraw.setPenColor(251, 252, 197);
+		double [] x = {0.3, 0.4, 0.6, 0.7};
+		double [] y = {0.2, 0.4, 0.4, 0.2};
+		
+		StdDraw.filledPolygon(x,y);
+		
+		
+
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,27 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		//rectangle 0 0 0 false 0.5 0.5 0.2 0.2
+		//triangle 36 255 36 true 0.1 0.1 0.5 0.9 0.9 0.1
+		//ellipse 146 0 0 true 0.5 0.5 0.2 0.2
+		String shape = in.next();
+		double r = in.nextDouble();
+		double g = in.nextDouble();
+		double b = in.nextDouble();
+		boolean fill = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double hWidth = in.nextDouble();
+		double hHeight = in.nextDouble();
+		if(shape == rectangle && fill == false) {
+			StdDraw.rectangle(x, y, hWidth, hHeight);
+		} else if(shape == rectangle && fill == true) {
+			StdDraw.setPenColor(r, g, b);
+			StdDrawfilled.rectangle(x, y, hWidth, hHeight);
+		}else if(shape == triangle && fill == false){
+			//
+
+		}
 		
 	}
 }
